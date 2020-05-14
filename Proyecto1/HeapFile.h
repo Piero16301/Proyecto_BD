994fd2840172a38536e9f74836e02de82a4e9a38
Piero Angelo <piero.morales@utec.edu.pe>
2020-05-14 17:37:48 -0500

    Creación de HeapFile

---
+++ Proyecto1/HeapFile.h
@@ -0,0 +1,8 @@
+#ifndef PROYECTO1_HEAPFILE_H
+#define PROYECTO1_HEAPFILE_H
+
+class HeapFile {
+    
+};
+
+#endif //PROYECTO1_HEAPFILE_H
--- Proyecto1/RandomFile.h
+++ Proyecto1/RandomFile.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <utility>
 #include <vector>
 #include <map>
 #include <utility>
@@ -14,9 +15,9 @@ class RandomFile {
 private:
     std::string indexFile;
     std::string dataFile;
-    int rowsIndexFile;
-    int sizeRecord;
-    int numDelRecords;
+    int rowsIndexFile{};
+    int sizeRecord{};
+    int numDelRecords{};
     std::map<int, int> indexRandomMap;
     std::map<int, pair<int, bool>> newIndexRandomMap;
 
@@ -162,8 +163,8 @@ public:
     RandomFile() = default;
 
     // Custom Constructor
-    RandomFile(std::string _dataFile){
-        dataFile = _dataFile;
+    explicit RandomFile(std::string _dataFile){
+        dataFile = std::move(_dataFile);
         indexFile = "../indexRandom.dat";
         rowsIndexFile = 0;
         sizeRecord = 85;
